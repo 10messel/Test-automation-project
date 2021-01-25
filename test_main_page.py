c28ab3951d70f7ac685717744b46f1188cc6e376
Denis Ivanov <ivanoffdenya@gmail.com>
2021-01-25 20:54:37 +0200

    First Page Object test

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,11 @@
 import time
+from .pages.main_page import MainPage
 
 
-def test_guest_should_see_login_link(browser):
-    browser.get("http://selenium1py.pythonanywhere.com/")
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
     time.sleep(3)
-    browser.find_element_by_id("login_link").click()
+    page.go_to_login_page()
     time.sleep(3)
